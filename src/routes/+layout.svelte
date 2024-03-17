4c3a97775e2224a6f87e6ca95df067968f06dde9
bourneidBH <bourneid87@gmail.com>
2024-03-17 16:01:30 -0500

    testing static deploy

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -8,6 +8,9 @@
 <svelte:head>
   <title>{`${$page.data.title} - Bourne Technology LLC`}</title>
   <meta name="description" content={$page.data.description} />
+  <link rel="preconnect" href="https://fonts.googleapis.com" />
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin="" />
+  <link href="https://fonts.googleapis.com/css2?family=Audiowide&family=Montserrat:ital,wght@0,400;0,700;1,400;1,700&display=swap" rel="stylesheet" />
   <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
   <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
   <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,11 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from "@sveltejs/adapter-static";
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+
+// use this for non-static site generation instead of @sveltjs/adapter-static
+// import adapter from '@sveltejs/adapter-auto';
+
+// https://sveltekitsaas.com/articles/sveltekit-github-pages-guide/
+const dev = process.env.NODE_ENV === "development"
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -11,7 +17,13 @@ const config = {
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter()
+		adapter: adapter({
+			pages: "docs",
+			assets: "docs"
+		}),
+		paths: {
+			base: dev ? "" : "/bourne-technology",
+		}
 	}
 };
 
