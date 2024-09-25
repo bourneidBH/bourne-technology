43755785082f6a7311c03fd4132b11f5f713863a
bourneidBH <bourneid87@gmail.com>
2024-09-25 12:00:00 -0500

    fixing build error

--- docs/_app/immutable/chunks/stores.S2PPr8Gp.js
+++
@@ -1,5 +0,0 @@
-<<<<<<<< HEAD:docs/_app/immutable/chunks/stores.4H8WuJpf.js
-import{s as e}from"./entry.FACwlXrz.js";const r=()=>{const s=e;return{page:{subscribe:s.page.subscribe},navigating:{subscribe:s.navigating.subscribe},updated:s.updated}},b={subscribe(s){return r().page.subscribe(s)}};export{b as p};
-========
-import{s as e}from"./entry.Kbjl6Zw3.js";const r=()=>{const s=e;return{page:{subscribe:s.page.subscribe},navigating:{subscribe:s.navigating.subscribe},updated:s.updated}},b={subscribe(s){return r().page.subscribe(s)}};export{b as p};
->>>>>>>> main:docs/_app/immutable/chunks/stores.S2PPr8Gp.js
